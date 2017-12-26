4a37b985a69526d055de51ea58fd31963a181fdb
Gaurav Gandhi <gauravgo4friends@gmail.com>
2017-12-27 02:36:12 +0530

    LastOccurence.py is modified

--- LastOccurence.py
+++ LastOccurence.py
@@ -1,26 +1,19 @@
 t = int(raw_input())
 while t > 0:
     n = int(raw_input())
-    arr = [int(x) for x in raw_input().strip().split(' ')]
+    arr = dict()
+    i = 1
+    a = [int(x) for x in raw_input().strip().split(' ')]
+    while i <= n:
+        arr[a[i-1]] = i
+        i+=1
+
     q = int(raw_input())
     while q > 0:
-        nn = int(raw_input())
-        i = 0
-        p = -1
-        while i < n:
-            if arr[i] == nn:
-                p = i
-                break
-            i+=1
-        if p == -1:
-            print p
-        else:
-            i = n-1
-            while i > p:
-                if arr[i] == nn:
-                    p = i
-                    break
-                i -= 1
-            print p+1
+        x = int(raw_input())
+        try:
+            print arr[x]
+        except KeyError:
+            print -1
         q-=1
     t-=1
